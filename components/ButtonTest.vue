8bbbcf8ded73a4464dac29150924e2901a8f68d6
pquinteros <pquinteros@gmail.com>
2023-08-12 16:41:42 -0300

    Cambia estilo en el componente de botón

--- components/ButtonTest.vue
+++ components/ButtonTest.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
+    <button class="bg-violet-700 hover:bg-violet-600 text-white font-semibold py-2 px-4 border border-violet-700 rounded">
       button
     </button>
   </div>
